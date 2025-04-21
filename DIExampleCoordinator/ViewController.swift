208b26fd13a6ed0648eab0817fe880a3bdcf9925
Melisa Gülgün Dombaycı <melissagulgun@gmail.com>
2025-04-21 20:37:15 +0300

    Initial Commit

---
+++ DIExampleCoordinator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DIExampleCoordinator
+//
+//  Created by Melisa Gülgün on 21.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DIExampleCoordinatorTests/DIExampleCoordinatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  DIExampleCoordinatorTests.swift
+//  DIExampleCoordinatorTests
+//
+//  Created by Melisa Gülgün on 21.04.2025.
+//
+
+import Testing
+@testable import DIExampleCoordinator
+
+struct DIExampleCoordinatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
